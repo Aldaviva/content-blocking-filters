4cb9bb4197db48f30ad8a0596eb47adac47991ec
Ben Hutchison <ben@aldaviva.com>
2025-06-03 05:12:26 -0700

    Fix hiding timeline interruptions like Who To Follow that occur at the very top of the timeline, where there are 1 fewer leading separators to match than when they are not at the top

--- build.fsx
+++ build.fsx
@@ -5,7 +5,7 @@ let utf8 = new UTF8Encoding(false, true)
 
 let lines = File.ReadAllLines("politics-patterns.txt", utf8)
 
-let buildFilterFile filename (title:string) (lineGenerator : string -> string) =
+let buildFilterFile (filename: string) (title: string) (lineGenerator: string -> string) =
     use outputStream = File.Create filename
     use writer = new StreamWriter(outputStream, utf8)
 
@@ -15,14 +15,12 @@ let buildFilterFile filename (title:string) (lineGenerator : string -> string) =
     
     for line in lines do
         writer.WriteLine()
-        writer.Write(lineGenerator(line))
+        writer.Write(lineGenerator line)
 
     printfn $"Wrote {filename}"
 
 buildFilterFile "filters/twitter-politics.txt" "Ben's Twitter Politics Filter" (fun keyword -> 
-    let selector = $"##div[aria-label='Timeline: Your Home Timeline'] > div > div > div > div:has(article[data-testid='tweet'] > div > div > div > div > div:nth-child(2):has-text({keyword})):style(opacity: 0; height: 0)"
-    
-    $"x.com{selector}")
+    $"x.com##div[aria-label='Timeline: Your Home Timeline'] > div > div > div > div:has(article[data-testid='tweet'] > div > div > div > div > div:nth-child(2):has-text({keyword})):style(opacity: 0; height: 0)")
 
 buildFilterFile "filters/bluesky-politics.txt" "Ben's Bluesky Politics Filter" (fun keyword -> 
     $"bsky.app##div[data-testid^='feedItem-by-']:has(:is(div[data-testid=postText], div[aria-label^='Post by '] > div[dir=auto]):has-text({keyword}))")
